bc3c35e4ef3236fcbe49c0104928a0894ae53714
MrRichGamer12 <100773555+MrRichGamer12@users.noreply.github.com>
2022-03-09 12:53:30 +0000

    Add files via upload

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -29,7 +29,7 @@ while (n[4] == n[0] || n[4] == n[1] || n[4] == n[2] || n[4] == n[3]) {
 while (s[0] == s[1]) {
     s[0] = Math.floor((Math.random() * 11) + 1)
 }
-for (i = 0; i < 4; i++) {
+for (i = 0; i < 5; i++) {
     let h1 = document.createElement("h1")
     tb.appendChild(h1)
     h1.append(`numero${i+1}-${n[i]}`)
