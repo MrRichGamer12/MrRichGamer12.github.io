9f6269d4d55a946ce99ab12d57f7f009f25b61f0
MrRichGamer12 <100773555+MrRichGamer12@users.noreply.github.com>
2022-03-09 11:24:50 +0000

    Add files via upload

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -1,32 +1,40 @@
-let n1=Math.floor((Math.random()*50)+1)
-let n2=Math.floor((Math.random()*50)+1)
-let n3=Math.floor((Math.random()*50)+1)
-let n4=Math.floor((Math.random()*50)+1)
-let n5=Math.floor((Math.random()*50)+1)
-let s1=Math.floor((Math.random()*11)+1)
-let s2=Math.floor((Math.random()*11)+1)
-while(n1==n2||n1==n3||n1==n4||n1==n5){
-    n1=Math.floor((Math.random()*50)+1)
+let tb=document.getElementById("div")
+let n=[
+    Math.floor((Math.random()*50)+1),
+    Math.floor((Math.random()*50)+1),
+    Math.floor((Math.random()*50)+1),
+    Math.floor((Math.random()*50)+1),
+    Math.floor((Math.random()*50)+1)
+]
+while(n[0]==n[1]||n[0]==n[2]||n[0]==n[3]||n[0]==n[4]){
+    n[0]=Math.floor((Math.random()*50)+1)
 }
-while(n2==n1||n2==n3||n2==n4||n2==n5){
-    n2=Math.floor((Math.random()*50)+1)
+while(n[1]==n[0]||n[1]==n[2]||n[1]==n[3]||n[1]==n[4]){
+    n[1]=Math.floor((Math.random()*50)+1)
 }
-while(n3==n2||n3==n1||n3==n4||n3==n5){
-    n3=Math.floor((Math.random()*50)+1)
+while(n[2]==n[1]||n[2]==n[1]||n[2]==n[1]||n[2]==n[1]){
+    n[2]=Math.floor((Math.random()*50)+1)
 }
-while(n4==n2||n4==n3||n4==n1||n4==n5){
-    n4=Math.floor((Math.random()*50)+1)
+while(n[3]==n[0]||n[3]==n[1]||n[3]==n[2]||n[3]==n[3]){
+    n[3]=Math.floor((Math.random()*50)+1)
 }
-while(n5==n2||n5==n3||n5==n4||n5==n1){
-    n5=Math.floor((Math.random()*50)+1)
+while(n[4]==n[0]||n[4]==n[1]||n[4]==n[2]||n[4]==n[3]){
+    n[4]=Math.floor((Math.random()*50)+1)
 }
-while(s1==s2){
-    s1=Math.floor((Math.random()*50)+1)
+let s=[
+    Math.floor((Math.random()*11)+1),
+    Math.floor((Math.random()*11)+1)
+]
+while(s[0]==s[1]){
+    s[0]=Math.floor((Math.random()*11)+1)
 }
-console.log(n1)
-console.log(n2)
-console.log(n3)
-console.log(n4)
-console.log(n5)
-console.log(s1)
-console.log(s2)
+for(i=0;i<4;i++){
+let h1=document.createElement("h1")
+tb.appendChild(h1)
+h1.append(n[i])
+}
+for(i=0;i<1;i++){
+    let p=document.createElement("p")
+    tb.appendChild(p)
+    p.append(s[i])
+    }
