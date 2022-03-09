18a56e1cc23eef104ee26c78c839535e6dd45385
MrRichGamer12 <100773555+MrRichGamer12@users.noreply.github.com>
2022-03-09 12:47:26 +0000

    Add files via upload

--- ex3/ex3.js
+++ ex3/ex3.js
@@ -6,35 +6,36 @@ let n = [
     Math.floor((Math.random() * 50) + 1),
     Math.floor((Math.random() * 50) + 1)
 ]
+let s = [
+    Math.floor((Math.random() * 11) + 1),
+    Math.floor((Math.random() * 11) + 1)
+]
 while (n[0] == n[1] || n[0] == n[2] || n[0] == n[3] || n[0] == n[4]) {
     n[0] = Math.floor((Math.random() * 50) + 1)
 }
 while (n[1] == n[0] || n[1] == n[2] || n[1] == n[3] || n[1] == n[4]) {
     n[1] = Math.floor((Math.random() * 50) + 1)
 }
-while (n[2] == n[1] || n[2] == n[1] || n[2] == n[1] || n[2] == n[1]) {
+while (n[2] == n[1] || n[2] == n[3] || n[2] == n[4] || n[2] == n[0]) {
     n[2] = Math.floor((Math.random() * 50) + 1)
 }
-while (n[3] == n[0] || n[3] == n[1] || n[3] == n[2] || n[3] == n[3]) {
+while (n[3] == n[0] || n[3] == n[1] || n[3] == n[2] || n[3] == n[4]) {
     n[3] = Math.floor((Math.random() * 50) + 1)
 }
 while (n[4] == n[0] || n[4] == n[1] || n[4] == n[2] || n[4] == n[3]) {
     n[4] = Math.floor((Math.random() * 50) + 1)
 }
-let s = [
-    Math.floor((Math.random() * 11) + 1),
-    Math.floor((Math.random() * 11) + 1)
-]
+
 while (s[0] == s[1]) {
     s[0] = Math.floor((Math.random() * 11) + 1)
 }
 for (i = 0; i < 4; i++) {
     let h1 = document.createElement("h1")
     tb.appendChild(h1)
-    h1.append(n[i])
+    h1.append(`numero${i+1}-${n[i]}`)
 }
-for (i = 0; i < 1; i++) {
+for (i = 0; i < 2; i++) {
     let p = document.createElement("p")
     tb.appendChild(p)
-    p.append(s[i])
+    p.append(`estrela${i+1}-${s[i]}`)
 }
